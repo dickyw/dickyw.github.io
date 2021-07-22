dece2bb80e34274096233b6c9efdef81e494a520
dickyw <dickyw@126.com>
2021-07-23 00:01:34 +0800

    摘星星游戏

---
+++ star-mobile/src/settings.5cfb7.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/game.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"1b082",main:"76276"}};
